b66d91070e02be985da2969ad7c553b9f87c4449
manuka_tharinda <bet21090@std.uwu.ac.lk>
2025-05-11 20:26:23 +0530

    first commit

---
+++ components/Home/Navbar/Mobilenavbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mobilenavbar = () => {
+  return (
+    <div>Mobilenavbar</div>
+  )
+}
+
+export default Mobilenavbar
---
+++ components/Home/Navbar/Responsivenavbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './Nav'
+import Mobilenavbar from './Mobilenavbar'
+
+const Responsivenavbar = () => {
+  return (
+    <>
+    
+    <Nav/>
+    <Mobilenavbar/>
+    
+    
+    
+    </>
+  )
+}
+
+export default Responsivenavbar
